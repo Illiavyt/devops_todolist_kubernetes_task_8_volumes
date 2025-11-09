0d078a5b92b67881e9a0a25a95c0f06271454f42
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-11-09 15:03:21 +0200

    fix a problem

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,17 @@
 kubectl create namespace mateapp --dry-run=client -o yaml | kubectl apply -f -
 
 # Створення PersistentVolume
-kubectl apply -f pv.yml -n mateapp
+kubectl apply -f .infrastructure/pv.yml -n mateapp
 
 # Створення PersistentVolumeClaim
-kubectl apply -f pvc.yml -n mateapp
+kubectl apply -f .infrastructure/pvc.yml -n mateapp
 
 # Застосування ConfigMap та Secret
-kubectl apply -f configmap.yml -n mateapp
-kubectl apply -f secret.yml -n mateapp
+kubectl apply -f .infrastructure/confgiMap.yml -n mateapp
+kubectl apply -f .infrastructure/secret.yml -n mateapp
 
 # Розгортання ToDo застосунку
-kubectl apply -f deployment.yml -n mateapp
+kubectl apply -f .infrastructure/deployment.yml -n mateapp
 
 # Усі ресурси створено
 kubectl get all -n mateapp
